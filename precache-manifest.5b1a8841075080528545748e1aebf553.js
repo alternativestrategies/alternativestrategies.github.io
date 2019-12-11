ce94b366de0a701db6d7df4e1f86f6b01e571d65
Arely Guevara <arelyguevara@Administrators-MacBook-Pro-2.local>
2019-12-10 19:42:41 -0500

    Updates

---
+++ precache-manifest.5b1a8841075080528545748e1aebf553.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04baf8cd01efb5f0fc8d0fe910dc74ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2a20249bab5e5e76113",
+    "url": "/static/css/2.ec0ec026.chunk.css"
+  },
+  {
+    "revision": "18400b6efb571564e54b",
+    "url": "/static/css/main.9902c39c.chunk.css"
+  },
+  {
+    "revision": "d2a20249bab5e5e76113",
+    "url": "/static/js/2.8fc95345.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.8fc95345.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18400b6efb571564e54b",
+    "url": "/static/js/main.9343d2cd.chunk.js"
+  },
+  {
+    "revision": "0c63a0543d3b02ce934a",
+    "url": "/static/js/runtime-main.a00d6c82.js"
+  }
+]);
